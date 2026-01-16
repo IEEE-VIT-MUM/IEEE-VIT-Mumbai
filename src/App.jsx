57d23fd7916305fac45449e95f680bb2c39ea135
Maitry Mohite <maitrymohite@vit.edu.in>
2026-01-16 16:18:16 +0530

    Initial commit for IEEE VIT Mumbai website

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import TeamSection from './components/TeamSection';
+
+function App() {
+  return (
+    <div className="App">
+      <TeamSection />
+    </div>
+  );
+}
+
+export default App;
