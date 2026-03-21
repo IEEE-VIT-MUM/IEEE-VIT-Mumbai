6eaa7f6a6a9326c52dea31372aee71bf53b97d53
Rishikesh <rishikeshpatil0605@gmail.com>
2026-03-21 11:11:09 +0530

    Rishikesh Patil: Deploy to GitHub Pages

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://gwncswephigcayqzyovn.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imd3bmNzd2VwaGlnY2F5cXp5b3ZuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI1MTcyMjEsImV4cCI6MjA4ODA5MzIyMX0.nfzurhMm2oVJd4UqItnDpEv3aNDPdOgLi1kWa1AF8c4';
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseAnon_Key = import.meta.env.VITE_SUPABASE_ANON_KEY
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
